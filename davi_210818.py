f4134f1add710682e3acde0a750768d4f64b5b18
davicova <86981409+davicova@users.noreply.github.com>
2021-08-18 23:33:22 -0300

    Add files via upload

---
+++ davi_210818.py
@@ -0,0 +1,11 @@
+words=["actually","again","almost","already","beside","luck","low"]
+print(words)
+print(words[0:1:1])
+print(words[6:7:6])
+print(words[2:5:1])
+print(words[1:7:2])
+print(words[-1:-7:-2])
+words.append("football")
+print(words)
+
+
